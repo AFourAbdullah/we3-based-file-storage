34580a89eb4ba37e9fdd6ce7dea3b0cee6f8fa9a
Abdullah Aziz <abdullahazees4@gmail.com>
2023-09-02 14:27:45 +0500

    code added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,18 +1,30 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 
 import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
+// import { GiHamburgerMenu } from "react/icons/gi";
+import { AiFillCloseCircle, AiOutlineMenu } from "react-icons/ai";
+
 import Link from "next/link";
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   const address = useAddress();
   return (
-    <header className="w-screen fixed top-0 left-0 z-50  bg-gradient-to-r from-blue-900 to-blue-700 flex items-center justify-between px-5">
+    <header className="w-screen fixed top-0  left-0 z-50  bg-gradient-to-r from-blue-900 to-blue-700 flex items-center justify-between px-5">
       <Link href="/">
         <img src="/logo.png" alt="" className="w-20 h-20 py-3 object-cover" />
       </Link>
 
       {address && (
-        <div className="flex text-white text-lg w-[300px] justify-between">
+        <div
+          className={`md:flex ${
+            isOpen ? "absolute" : "hidden"
+          } a text-white text-lg md:w-[300px] w-screen bg-blue-900 flex-col md:flex-row items-center justify-between`}
+        >
           <Link
             href="/myfiles"
             className="mr-5 hover:text-gray-400 hover:underline"
@@ -25,6 +37,13 @@ const Header = () => {
         </div>
       )}
       <ConnectWallet theme="dark" className="connectBtn " />
+      <div className="md:hidden flex items-center justify-center w-[30px] text-white text-lg">
+        {isOpen ? (
+          <AiFillCloseCircle onClick={() => toggleMenu()} />
+        ) : (
+          <AiOutlineMenu onClick={() => toggleMenu()} />
+        )}
+      </div>
     </header>
   );
 };
